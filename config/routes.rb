c40a0e9dc0b95ca4d28ee3cc8e919842508b91ff
Sarina Ke <sarina.y.ke@gmail.com>
2022-08-17 14:18:27 +0800

    finished rails task manager challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tasks
+
+  # # GET ALL TASKS
+  # get 'tasks', to: 'tasks#index', as: :tasks
+
+  # # CREATE NEW TASK
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # post "tasks", to: "tasks#create"
+
+  # # GET SPECIFIC TASK
+  # get "tasks/:id", to: "tasks#show", as: :task
+
+  # # UPDATE NEW TASK
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+
+  # # DELETE TASK
+  # delete "tasks/:id", to: "tasks#destroy", as: :destroy_task
 end
